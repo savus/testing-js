1c9ca26a6fac818de01a1a7660a160216a57a71c
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2025-11-25 06:24:04 -0700

    add dropdown menu to nav link that does not deactivate unless blurred

--- js/index.js
+++ js/index.js
@@ -17,3 +17,21 @@ navBar.addEventListener("click", navBarHandler);
 closeButtons.forEach((button) => {
   button.addEventListener("click", closeButtonOnClick);
 });
+
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  const dropDownParent = e.target.closest("[data-dropdown]");
+  console.log(isDropdownButton, dropDownParent);
+  if (!isDropdownButton && dropDownParent != null) return;
+
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle(active);
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
